19fdc8b6c9a2da9d689f3061df3cded2292c41c5
chris_code <chris_code@sdsia.laas.fr>
2026-06-08 22:32:17 +0200

    comment test

--- tests/relevant_score_test.sql
+++
@@ -1,8 +0,0 @@
--- SELECT
---   user_id,
---   movie_id,
---   relevance_score
--- FROM {{ ref('fct_genome_scores') }}
--- WHERE relevance_score > 0.5
-
-{# {{ nextflix.no_nulls_in_columns(ref('fct_genome_scores')) }} #}
